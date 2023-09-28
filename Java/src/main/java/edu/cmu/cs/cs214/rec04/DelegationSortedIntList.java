df8b1029568ff0220ce1bf859b5742535cc6808c
rmdettmar <rmdettmar@gmail.com>
2023-09-28 00:59:11 -0400

    [java] sorted int list variants

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
@@ -10,7 +10,42 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class DelegationSortedIntList {
+public class DelegationSortedIntList implements IntegerList {
     // Write your implementation below with API documentation
+    private IntegerList sortlst = new SortedIntList();
+    private int totalAdded = 0;
 
+    public boolean add(int num) {
+        totalAdded++;
+        if(sortlst.add(num)) return true;
+        else return false;
+    }
+
+    public boolean addAll(IntegerList list) {
+        totalAdded += list.size();
+        if (sortlst.addAll(list)) return true;
+        else return false;
+    }
+
+    public int get(int index) {
+        return sortlst.get(index);
+    }
+
+    public boolean remove(int num) {
+        if (sortlst.remove(num)) return true;
+        else return false;
+    }
+
+    public boolean removeAll(IntegerList list) {
+        if (sortlst.removeAll(list)) return true;
+        else return false;
+    }
+
+    public int size() {
+        return sortlst.size();
+    }
+
+    public int getTotalAdded() {
+        return totalAdded;
+    }
 }
--- Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
@@ -10,7 +10,45 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class InheritanceSortedIntList {
+public class InheritanceSortedIntList extends SortedIntList {
     // Write your implementation below with API documentation
+
+    private int totalAdded = 0;
+
+    /**
+     * Adds the specified int to the list. Overrides the inherited version.
+     *
+     * @param num an integer to be added to the list
+     * @return true if the list is changed as a result of the call
+     */
+    @Override
+    public boolean add(int num){
+        this.totalAdded++;
+        if(super.add(num)) return true;
+        else return false;
+    }
+
+    /**
+     * Adds all of the elements of the IntegerList to the list.
+     * Overrides the inherited version.
+     *
+     * @param list IntegerList containing elements to be added to the list
+     * @return true if the list changed as a result of the call
+     */
+    /* @Override
+    public boolean addAll(IntegerList list){
+        if(super.addAll(list)) return true;
+        else return false;
+    } */
+
+    /**
+     * Returns the total number of elements that have been added to the list
+     * since its creation.
+     * 
+     * @return the total number of elements added since creation
+     */
+    public int getTotalAdded(){
+        return totalAdded;
+    }
 
 }
